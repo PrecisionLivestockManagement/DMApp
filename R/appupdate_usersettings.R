4f21efb040b99ce2adbfcbdd58eb2e3db14fb8a6
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-02-24 14:56:13 +1000

    New function to update user settings from the DM Website

---
+++ R/appupdate_usersettings.R
@@ -0,0 +1,30 @@
+#' Retrieves user settings from the DataMuster database
+#'
+#' This function allows user settings to be retreived from the DataMuster database via the DataMuster website app
+#' @name appupdate_usersettings
+#' @param email the email address of the registered user
+#' @param username a username to access the DataMuster database, contact Lauren O'Connor for database access
+#' @param password a password to access the DataMuster database
+#' @return a dataframe showing the information associated with the user settings
+#' @author Dave Swain \email{d.swain@@cqu.edu.au} and Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}
+#' @import mongolite
+#' @import keyring
+#' @export
+
+
+appupdate_usersettings <- function(email, language = NULL, lastprop = NULL, username=NULL, password=NULL){
+
+  if(is.null(username)||is.null(password)){
+  username = keyring::key_list("DMMongoDB")[1,2]
+  password =  keyring::key_get("DMMongoDB", username)
+}
+
+  pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
+  usersettings <- mongo(collection = "UserSettings", db = "DataMuster", url = pass, verbose = T)
+
+  usersinfo <- sprintf('{"User":"%s"}', isolate(email))
+  datainfo <- sprintf('{"$set":{"Language":"%s", "LastProperty":"%s", "createdAt":{"$date":"%s"}}}', isolate(language), isolate(propchoice), paste0(substr(Sys.time(),1,10),"T",substr(Sys.time(),12,19),"+1000"))
+
+  usersettings$update(usersinfo, datainfo, upsert = TRUE)
+
+}
