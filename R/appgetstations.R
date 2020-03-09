993a0a96a825e96ca4a97d7d65d138ce91fa6988
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-03-09 10:13:13 +1000

    Brought up to date

--- R/appgetstations.R
+++ R/appgetstations.R
@@ -3,26 +3,33 @@
 #' This function retrieves property information from the DataMuster database via the DataMuster website
 #' @name  appgetstations
 #' @param property a list of property names, if NULL all active properties in the database will be returned
+#' @param accesslevel the users database accesslevel, either "admin" or "user"
 #' @param username a username to access the DataMuster database
 #' @param password a password to access the DataMuster database
-#' @return a dataframe showing property names
+#' @return a dataframe showing property names and timezone
 #' @author Dave Swain \email{d.swain@@cqu.edu.au} and Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}
 #' @import mongolite
+#' @import dplyr
 #' @export
 
 
-appgetstations <- function(property=NULL, username, password){
+appgetstations <- function(property, accesslevel, username, password){
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   station <- mongo(collection = "Stations", db = "DataMuster", url = pass, verbose = T)
 
-  lookfor <- sprintf('{"stationname":true, "_id":false}')
+  lookfor <- sprintf('{"stationname":true, "timezone":true, "_id":false}')
 
-  if (is.null(property)){
+  if (accesslevel == "admin"){
     filter <- sprintf('{"active":"%s"}', "TRUE")}else{
+    property <- paste(unlist(property), collapse = '", "' )
     filter <- sprintf('{"active":"%s", "stationname":{"$in":["%s"]}}', "TRUE", property)}
 
   propertyinfo <- station$find(query = filter, fields = lookfor)
+
+  propertyinfo <- propertyinfo%>%
+                  select(stationname, timezone)%>%
+                  arrange(stationname)
 
   return(propertyinfo)
 
