dfc03893626baedb35d6dbec68e4e5f9345e9862
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-03-12 10:00:27 +1000

    Fixed filtering on alms unit

--- R/appalmsusegraph.R
+++ R/appalmsusegraph.R
@@ -22,7 +22,7 @@ appalmsusegraph <- function(property, start, sex, category, alms, timezone, user
   property <- sprintf('"Property":"%s",', property)
   if(sex == "all"){sex <- NULL} else {sex <- sprintf('"Sex":"%s",', sex)}
   if(category == "all"){category <- NULL} else {category <- sprintf('"Category":"%s",', category)}
-  if(alms == "All units"){alms <- NULL} else {alms <- sprintf('"ALMS":"%s",', alms)}
+  if(alms == "All units" | alms == "No Data"){alms <- NULL} else {alms <- sprintf('"ALMS":"%s",', alms)}
   start <- sprintf('"Date":{"$gte":{"$date":"%s"}},', strftime(as.POSIXct(paste0(start, "00:00:00")), format="%Y-%m-%dT%H:%M:%OSZ", tz = "GMT"))
 
   # Set up query to search for data
@@ -34,6 +34,8 @@ appalmsusegraph <- function(property, start, sex, category, alms, timezone, user
   lookfor <- sprintf('{"Date":true, "Category":true, "Sex":true, "Count":true, "_id":false}')
 
   almsinfo <- almsuse$find(query = filter, fields = lookfor)
+
+  if(nrow(almsinfo) == 0){allcattle <- data.frame()}else{
 
   breeding <- almsinfo%>%
               filter(Category == "breeding")%>%
@@ -57,6 +59,7 @@ appalmsusegraph <- function(property, start, sex, category, alms, timezone, user
   allcattle <- allcattle%>%
                ungroup()%>%
                mutate(Date = as.character(as.Date(Date, tz = timezone), format = "%b %d"))
+  }
 
   return(allcattle)
 
