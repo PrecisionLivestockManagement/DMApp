5d6ebe1f4cb57d3073e279f50361f1e85ee83506
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-06-04 14:41:57 +1000

    appgetpaddocks.R - removed some fields that are returned, no longer required

--- R/appgetpaddocks.R
+++ R/appgetpaddocks.R
@@ -18,7 +18,7 @@ appgetpaddocks <- function(property, username, password){
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   paddocks <- mongo(collection = "Paddocks", db = "DataMuster", url = pass, verbose = T)
 
-  lookfor <- sprintf('{"stationname":true, "geometry.coordinates":true, "properties.hectares":true, "paddname":true, "cattle":true, "AE":true, "breeding":true, "growing":true,
+  lookfor <- sprintf('{"stationname":true, "geometry.coordinates":true, "properties.hectares":true, "paddname":true,
                      "ALMSrating":true, "LTCC_A":true, "LTCC_B":true, "LTCC_C":true, "LTCC_D":true, "_id":false}')
   filter <- sprintf('{"stationname":"%s"}', property)
 
@@ -27,12 +27,12 @@ appgetpaddocks <- function(property, username, password){
   tempadds <- tempadds%>%
               mutate(hectares = properties$hectares,
               geom = geometry$coordinates)%>%
-              select("stationname", "paddname", "hectares", "LTCC_A", "LTCC_B", "LTCC_C", "LTCC_D", "ALMSrating", "cattle", "AE", "breeding", "growing", "geom")
+              select("stationname", "paddname", "hectares", "LTCC_A", "LTCC_B", "LTCC_C", "LTCC_D", "ALMSrating", "geom")
 
   cattle = SpatialPolygons(lapply(1:nrow(tempadds), function(x) Polygons(list(Polygon(matrix(tempadds$geom[[x]], ncol = 2))), paste0("ID",x))), proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
   cattleadd <- tempadds%>%
-               select(stationname, paddname, hectares, LTCC_A, LTCC_B, LTCC_C, LTCC_D, ALMSrating, cattle, AE, breeding, growing)
+               select(stationname, paddname, hectares, LTCC_A, LTCC_B, LTCC_C, LTCC_D, ALMSrating)
 
   PropPadds <- SpatialPolygonsDataFrame(cattle, cattleadd, match.ID=FALSE)
 
