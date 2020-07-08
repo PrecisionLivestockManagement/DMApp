6a583b87b70b1e089770d02008bf58e9bdff6dec
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-07-08 14:27:00 +1000

    appgetinfrastructure.R - added timezone as an input variable and formated lastsignal to the appropriate property timezone

--- R/appgetinfrastructure.R
+++ R/appgetinfrastructure.R
@@ -3,6 +3,7 @@
 #' This function retreives a list of ALMS infrastructure for a property for configuration of the DataMuster website
 #' @name  appgetinfrastructure
 #' @param property the name of the property to search the database
+#' @param timezone the timezone of the property to display the weekly weight data
 #' @param username a username to access the DataMuster database
 #' @param password a password to access the DataMuster database
 #' @return a dataframe of infrastructure and associated information
@@ -12,22 +13,24 @@
 #' @export
 
 
-appgetinfrastructure <- function(property, username, password){
+appgetinfrastructure <- function(property, timezone, username, password){
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   infrastructure <- mongo(collection = "Infrastructure", db = "DataMuster", url = pass, verbose = T)
 
   property <- sprintf('"stationname":"%s",', property)
   active <- sprintf('"properties.datarecording":"%s",', "TRUE")
-  type <- sprintf('"properties.type":"%s",', "Walk-over-Weighing Unit")
+
+  currenttime <- Sys.time()
+  attr(currenttime,"tzone") <- timezone
 
   # Set up query to search for cattle
 
-  filter <- paste0("{", property, active, type,"}")
+  filter <- paste0("{", property, active,"}")
   filter <- substr(filter, 1 , nchar(filter)-2)
   filter <- paste0(filter, "}")
 
-  lookfor <- sprintf('{"stationname":true, "properties.asset_id":true, "properties.filename":true, "properties.lastsignal":true, "properties.Paddock":true, "_id":false}')
+  lookfor <- sprintf('{"stationname":true, "properties.asset_id":true, "properties.type":true, "properties.filename":true, "properties.lastsignal":true, "properties.Paddock":true, "_id":false}')
 
   infsinfo <- infrastructure$find(query = filter, fields = lookfor)
 
@@ -36,7 +39,8 @@ appgetinfrastructure <- function(property, username, password){
   infsinfo <- cbind(infsinfo[-1], infsinfo$properties)
 
   infsinfo <- infsinfo%>%
-              mutate(status = ifelse(as.numeric(Sys.time() - lastsignal) < 60, "Active", ifelse(as.numeric(Sys.time() - lastsignal) <= 180, "Check", "Not Active"))) %>%
+              mutate(lastsignal = format(as.POSIXct(lastsignal), tz = timezone),
+              status = ifelse(as.numeric(difftime(currenttime, lastsignal, units = "mins")) < 60, "Active", ifelse(as.numeric(difftime(currenttime, lastsignal, units = "mins")) <= 180, "Check", "Not Active"))) %>%
               filter(asset_id != "xxxxxx")
 
   return(infsinfo)
