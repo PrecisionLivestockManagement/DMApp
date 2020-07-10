fee6b38614a27d5e1ce720cdf73e2df57f946f30
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-07-10 14:24:44 +1000

    appgetcattle.R - updated code to bring in cattle information without spatial referencing

--- R/appgetcattle.R
+++ R/appgetcattle.R
@@ -9,7 +9,7 @@
 #' @param paddock the paddock allocation of the cattle to be returned, determined by selecting a paddock on the map
 #' @param username a username to access the DataMuster database
 #' @param password a password to access the DataMuster database
-#' @return a spatialpointsdataframe with a list of cattle RFID numbers and associated coordinates
+#' @return a dataframe with a list of cattle numbers by paddock
 #' @author Dave Swain \email{d.swain@@cqu.edu.au} and Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}
 #' @import mongolite
 #' @import dplyr
@@ -21,32 +21,60 @@ appgetcattle <- function(property, sex, category, paddock, zoom, username, passw
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
+  paddocks <- mongo(collection = "Paddocks", db = "DataMuster", url = pass, verbose = T)
 
-  property <- sprintf('"stationname":"%s",', property)
+  stationname <- sprintf('"stationname":"%s",', property)
   if(sex == "all"){sex <- NULL} else {sex <- sprintf('"properties.sex":"%s",', sex)}
   if(category == "all"){category <- NULL} else {category <- sprintf('"properties.category":"%s",', category)}
   if(is.null(paddock)||zoom == 1){paddock <- NULL}else{paddock <- sprintf('"properties.Paddock":"%s",', paddock)}
 
   # Set up query to search for cattle
 
-  filter <- paste0("{", property, sex, category, paddock,"}")
+  filter <- paste0("{", stationname, sex, category, paddock,"}")
   filter <- substr(filter, 1 , nchar(filter)-2)
   filter <- paste0(filter, "}")
 
-  lookfor <- sprintf('{"RFID":true, "geometry":true, "_id":false}')
+  lookfor <- sprintf('{"properties.Paddock":true, "properties.AE":true, "properties.category":true, "properties.stweight":true, "properties.stwtdate":true,
+                     "properties.wkweight":true, "properties.wkwtdate":true, "_id":false}')
 
   cattleinfo <- cattle$find(query = filter, fields = lookfor)
 
   if(nrow(cattleinfo) == 0){
     cattleinfo <- cattle$find(query = sprintf('{"RFID":"xxxxxx"}'), fields = lookfor)}
 
-  cattleinfo <- cattleinfo%>%
-                mutate(geom = geometry$coordinates)
+  cattleinfo1 <- cattleinfo$properties %>%
+                 mutate(weight = ifelse(wkwtdate >= stwtdate, wkweight, stweight),
+                        weightdate = ifelse(wkwtdate >= stwtdate, as.character(wkwtdate), as.character(stwtdate)),
+                        weightdate = as.Date(weightdate),
+                        weight = ifelse(weight == 0, NA, weight))
 
-  cattleinfo <- cattleinfo[-1]
+  countcattle <- cattleinfo1 %>%
+                 group_by(Paddock, category) %>%
+                 summarise(cattle = n(),
+                           AE = round(sum(AE),0),
+                           avweight = round(mean(as.numeric(weight), na.rm = T),0),
+                           lastdate = max(weightdate)) %>%
+                 mutate(avweight = ifelse(is.nan(avweight), NA, avweight))
 
-  cattleinfospatial <- SpatialPointsDataFrame(data.frame(matrix(unlist(cattleinfo$geom), nrow=length(cattleinfo$geom), byrow=T)), cattleinfo%>%select(-"geom"))
+  # paddocks1 <- DMApp::appgetpaddocks(property = property, username = username, password = password)
+  #
+  # for(i in 1:nrow(paddocks1@data)){
+  #
+  #   lat <- paddocks1@polygons[[i]]@labpt[1]
+  #   long <- paddocks1@polygons[[i]]@labpt[2]
+  #
+  #   paddocks1$lat[i] <- paddocks1@polygons[[i]]@labpt[1]
+  #   paddocks1$long[i] <- paddocks1@polygons[[i]]@labpt[2]
+  #
+  # }
+  #
+  # paddocks1 <- data.frame(paddocks1 %>%
+  #              select(paddname, lat, long)) %>%
+  #              rename(Paddock = paddname)
+  #
+  # cattlepaddata <- data.frame(left_join(countcattle, paddocks1, by = "Paddock") %>%
+  #                  filter(Paddock != "xxxxxx"))
 
-  return(cattleinfospatial)
+  return(countcattle)
 
 }
