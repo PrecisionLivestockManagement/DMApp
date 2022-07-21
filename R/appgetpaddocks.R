a15c3ed23eacc913f3919e96a9c03e9649f81824
Lauren O'Connor <l.r.oconnor@cqu.edu.au>
2022-07-21 14:10:42 +1000

    Replacing appgetpaddocklist with appgetpaddocks

--- R/appgetpaddocks.R
+++ R/appgetpaddocks.R
@@ -1,7 +1,7 @@
 #' Retrieves paddock information from the DataMuster database
 #'
 #' This function retrieves a list of paddock polygons for display on the property map on the DataMuster website
-#' @name appgetpaddocklist
+#' @name appgetpaddocks
 #' @param property the name of the property to search the database
 #' @param username a username to access the DataMuster database
 #' @param password a password to access the DataMuster database
@@ -13,10 +13,10 @@
 #' @export
 
 
-appgetpaddocklist <- function(property, username, password){
+appgetpaddocks <- function(property, username, password){
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
-  paddocklist <- mongo(collection = "Paddocklist", db = "DMApp", url = pass, verbose = T)
+  paddocks <- mongo(collection = "Paddocks", db = "DataMuster", url = pass, verbose = T)
 
   property <- sprintf('"stationname":"%s",', property)
 
@@ -26,9 +26,14 @@ appgetpaddocklist <- function(property, username, password){
   filter <- substr(filter, 1 , nchar(filter)-2)
   filter <- paste0(filter, "}")
 
-  lookfor <- sprintf('{"_id":false}')
+  lookfor <- sprintf('{"stationname":true, "geometry.coordinates":true, "properties.hectares":true, "paddname":true, "cattle":true, "_id":false}')
 
-  padds <- paddocklist$find(query = filter, fields = lookfor)
+  padds <- paddocks$find(query = filter, fields = lookfor)
+
+  padds <- padds %>%
+    mutate(hectares = properties$hectares,
+           geom = geometry$coordinates)%>%
+    select("stationname", "paddname", "hectares", "cattle", "geom")
 
   paddinfo <- subset(padds, select = -geom)
 
